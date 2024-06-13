7d6164ca70ecdbf265db0f7d5f0999e953445a69
Simone Nerucci <102528993+SimNeru@users.noreply.github.com>
2024-06-13 10:26:32 +0200

    Initializing project

---
+++ src/main/java/com/simeru/EardreamsApplication.java
@@ -0,0 +1,13 @@
+package com.simeru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EardreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EardreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simeru/EardreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simeru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EardreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
